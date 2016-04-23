6ddbf2e9d718ca3e726f1107f090988fe60b63b9
Aredhel23 <Aciredef@Craig>
2016-04-23 11:16:20 +0200

    ReservationRegister Test with new method tested

--- ProgettoPrenotazioneAule/src/Test/TestReservationRegister.java
+++ ProgettoPrenotazioneAule/src/Test/TestReservationRegister.java
@@ -17,23 +17,24 @@ import java.util.GregorianCalendar;
 public class TestReservationRegister {
     public static void main(String[] args) {
         Calendar ca = new GregorianCalendar();
-        ca.set(3, 5, 2016);
+        ca.set(2016, 5, 5);
         
         
         
         ReservationRegister r = new ReservationRegister("EF4");
         r.isReserved(ca, 9,11);
-        System.out.println(r.isReserved(ca,9,11));
-        System.out.println(r.makeReservation(ca,9,11));
-        System.out.println(r.makeReservation(ca,9,11));
-        r.isReserved(ca ,9,11);
-        System.out.println(r.isReserved(ca ,9,11));
-        System.out.println(r.makeReservation(ca,11,12));
-        System.out.println(r.isReserved(ca ,9,11));
-        System.out.println(r.isReserved(ca ,7,10));
-        System.out.println(r.makeReservation(ca,7,11));
-        System.out.println(r.makeReservation(ca,12,14));
-        System.out.println(r.makeReservation(ca,13,17));
-        System.out.println(r.makeReservation(ca,9,17));
+        System.out.println(r.isReserved(ca,9,11));//false
+        System.out.println(r.makeReservation(ca,9,11)); //true
+        System.out.println(r.makeReservation(ca,9,11)); //false
+        r.isReserved(ca ,9,11);     
+        System.out.println(r.isReserved(ca ,9,11)); //true
+        System.out.println(r.makeReservation(ca,11,12)); //true
+        System.out.println(r.isReserved(ca ,9,11));     //true
+        System.out.println(r.isReserved(ca ,7,10));     //true
+        System.out.println(r.makeReservation(ca,7,11));//false
+        System.out.println(r.makeReservation(ca,12,14));//true
+        System.out.println(r.makeReservation(ca,13,17)); //false
+        System.out.println(r.makeReservation(ca,9,17));  //false
+        r.printRegister();
     }
 }

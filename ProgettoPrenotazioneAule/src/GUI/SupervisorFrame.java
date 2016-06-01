6472e4907e49eb19357f821d3806aff3677ac6ba
Aredhel23 <Aciredef@10.87.249.61>
2016-06-01 11:02:16 +0200

    GUI Classes and Test Classes Modified

--- ProgettoPrenotazioneAule/src/GUI/SupervisorFrame.java
+++ ProgettoPrenotazioneAule/src/GUI/SupervisorFrame.java
@@ -5,12 +5,39 @@
  */
 package GUI;
 
+import PrenotareAula.Campus;
+import java.awt.HeadlessException;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  *
  * @author Aciredef
  */
 public class SupervisorFrame extends JFrame {
+    private Campus c;
+    private String email;
+    private JLabel name;
+    private JLabel selection;
+    private JButton newReservation;
+    private JButton weeklyReservation;
+    private JButton deleteReservation;
+    private JButton editReservation;
+    private JButton printReservation;
+    private JButton addTeacher;
+    private JButton exit;
+
+    public SupervisorFrame(String email) throws HeadlessException {
+        this.setVisible(true);
+        this.email = email;
+        initcomponents();
+        
+    }
     
+    public void initcomponents() {
+        c = Campus.getInstance();
+        name = new JLabel ( "Benvenuto Gestore: " +email);
+        selection = new JLabel("Scegli un'opzione tra le seguenti:" );
+    }
 }
